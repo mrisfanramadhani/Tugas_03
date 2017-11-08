9154d60e1bbba8b4a6913db4952a66f848bce703
mrisfanramadhani <32300986+mrisfanramadhani@users.noreply.github.com>
2017-11-08 22:42:06 +0800

    Penyelesaian

--- Tugas_03.cpp
+++ Tugas_03.cpp
@@ -20,4 +20,18 @@ int main(){
 		gaber=gtot-(0.15*gtot);
 		cout<<"Gaji Brsih \t\t = "<<"Rp "<<gaber;
 	}
+	else if(jk<=40){
+		cout<<"Gaji Pokok \t\t =  Rp 2.000.000"<<endl;
+		cout<<"Jam Kerja Standart\t = 40 jam"<<endl;
+		lb=0;
+		cout<<"Lembur\t\t\t = "<<lb<<" Jam"<<endl;
+		jl=lb*1.5;
+		jtot=40+jl;
+		cout<<"Total Jam Kerja\t\t = "<<jtot<<" jam"<<endl;
+		gtot=2000000+(jl*100000);
+		cout<<"Total Gaji \t\t = 15%"<<endl;
+		gaber=gtot-(0.15*gtot);
+		cout<<"Gaji Bersih\t\t = "<<" Rp"<<gaber;
+	}
+	return 0;
 }
